c08db75c5df77e384d5e0a7d53569e4bebb78447
henriquesdias <henrique.dias1915@gmail.com>
2023-03-03 12:32:37 -0300

    feat: the informations about article are sabe in server

--- script.js
+++ script.js
@@ -14,8 +14,15 @@ function createParagraph() {
 function createSection() {
   const boxInput = document.querySelector("#input");
   const description = document.querySelector("input").value;
+  let type = "";
+  if (boxInput.parentNode.tagName === "DIV") {
+    type = "paragraph";
+  } else {
+    type = "title";
+  }
   boxInput.parentNode.innerHTML = description;
   boxInput.classList.add("hide-item");
+  putRequest({ type, content: description });
 }
 function createTitle() {
   const article = document.querySelector("article");
@@ -44,6 +51,7 @@ function createSectionwithImage() {
   boxInput.nextElementSibling.classList.remove("hide-item");
   boxInput.nextElementSibling.src = description;
   boxInput.remove();
+  putRequest({ type: "img", content: description });
 }
 function createLine() {
   const article = document.querySelector("article");
@@ -52,6 +60,7 @@ function createLine() {
     <div></div>
   </div>
   `;
+  putRequest({ type: "line" });
 }
 function drag(e) {
   e.dataTransfer.setData("firstElement", e.target.outerHTML);
@@ -68,4 +77,54 @@ function drop(e) {
   const data = e.dataTransfer.getData("firstElement");
   e.target.insertAdjacentHTML("beforeBegin", data);
   e.dataTransfer.clearData();
+}
+function putRequest(body) {
+  const URL = "http://localhost:4001/api/getArticle";
+  fetch(URL, {
+    mode: "cors",
+    method: "PUT",
+    body: JSON.stringify(body),
+  })
+    .catch((res) => console.log(res))
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+}
+function getRequest() {
+  const URL = "http://localhost:4001/api/getArticle";
+  return fetch(URL, {
+    mode: "cors",
+  });
+}
+loadingArticle();
+function loadingArticle() {
+  const article = document.querySelector("article");
+  getRequest()
+    .catch((res) => console.log(res))
+    .then((response) => response.json())
+    .then((data) =>
+      data.forEach((e) => {
+        if (e.type === "title") {
+          article.innerHTML += `
+          <h1 draggable="true" ondragover="allowDrop(event)" ondrop="drop(event)" ondragstart="drag(event)">
+          ${e.content}
+          </h1>`;
+        }
+        if (e.type === "paragraph") {
+          article.innerHTML += `
+          <div class="paragraph" draggable="true" ondragover="allowDrop(event)" ondrop="drop(event)" ondragstart="drag(event)">
+            ${e.content}
+          </div>
+          `;
+        }
+        if (e.type === "line") {
+          article.innerHTML += `
+          <div class="line" draggable="true" ondragover="allowDrop(event)" ondrop="drop(event)" ondragstart="drag(event)">
+          <div></div>
+        </div>`;
+        }
+        if (e.type === "img") {
+          article.innerHTML += `<img src="${e.content}" alt="image" class="img" draggable="true" ondragover="allowDrop(event)" ondrop="drop(event)" ondragstart="drag(event)">`;
+        }
+      })
+    );
 }
